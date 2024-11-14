45a70912821cb2a7cfaee43d79545b8e7a9e39cf
renatha <166110072+Cutrenatha@users.noreply.github.com>
2024-11-14 17:07:26 +0700

    Update AdminDriver.java

--- models/AdminDriver.java
+++ models/AdminDriver.java
@@ -2,13 +2,21 @@ package models;
 
 import java.util.ArrayList;
 
+// Kelas AdminDriver mengimplementasikan fungsi pengelolaan transaksi dan barang oleh admin
 public class AdminDriver extends Driver {
+    // listBarang mewakili koleksi barang yang dikelola oleh admin
     private ListBarang listBarang;
+    // listTransaksi adalah daftar transaksi yang diproses oleh admin
     private ArrayList<Transaksi> listTransaksi;
 
+    
+    // constructor AdminDriver menerima objek admin dan koleksi barang
     public AdminDriver(Admin admin, ListBarang listBarang) {
+        // Memanggil konstruktor superclass (Driver) dengan parameter admin
         super(admin);
+        // Menginisialisasi listBarang dengan koleksi barang yang diterima
         this.listBarang = listBarang;
+        // Menginisialisasi listTransaksi sebagai daftar kosong
         this.listTransaksi = new ArrayList<>();
     }
 
