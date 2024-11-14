cd12e2f96e5a0fa46698113f75d558454ba44962
renatha <166110072+Cutrenatha@users.noreply.github.com>
2024-11-14 14:20:41 +0700

    Update Admin.java

--- models/Admin.java
+++ models/Admin.java
@@ -1,10 +1,15 @@
 package models;
 
+// Kelas Admin adalah subclass dari kelas Akun, yang mewakili akun dengan peran Admin
 public class Admin extends Akun {
+       
+    // Konstruktor kelas Admin, menerima id, username, dan password sebagai parameter
     public Admin(String id, String username, String password) {
+    // Memanggil konstruktor superclass (Akun) untuk menginisialisasi atribut
         super(id, username, password);
     }
-
+    
+    // Override metode getRole untuk mengembalikan peran sebagai "Admin"
     @Override
     public String getRole() {
         return "Admin";
