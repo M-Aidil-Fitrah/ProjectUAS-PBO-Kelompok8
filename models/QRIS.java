666a6da539066a756422e6f4b21a684799c730c2
renatha <166110072+Cutrenatha@users.noreply.github.com>
2024-11-14 17:25:44 +0700

    Update QRIS.java

--- models/QRIS.java
+++ models/QRIS.java
@@ -1,12 +1,12 @@
 package models;
 
+// Kelas QRIS merupakan turunan dari kelas Pembayaran dan menangani pembayaran melalui metode QRIS
 public class QRIS extends Pembayaran {
+
+    // Konstruktor QRIS yang menerima ID pembayaran
     public QRIS(String id) {
+        // Memanggil konstruktor superclass (Pembayaran) dengan ID yang diberikan
         super(id);
     }
 
-    @Override
-    public void prosesPembayaran() {
-        System.out.println("Pembayaran via QRIS berhasil.");
-    }
-}
+ 
